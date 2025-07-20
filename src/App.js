4577524aea212a25d76dd481ef7f36bc73c16686
Preeta Puhan <preetapuhan1@gmail.com>
2025-07-20 10:59:47 +0530

    Initial commit - React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Doctor Job Listings</h1>
+        <p>Find the best doctor jobs with filtering and bookmarking.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
